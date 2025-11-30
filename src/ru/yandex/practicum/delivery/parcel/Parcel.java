dcba9f8e865452963263995934893a8cab26064b
Roman Udalov <roman.ud5@yandex.ru>
2025-11-30 17:54:15 +0300

    Готовый финальный проект спринта №5

---
+++ src/ru/yandex/practicum/delivery/parcel/Parcel.java
@@ -0,0 +1,74 @@
+package ru.yandex.practicum.delivery.parcel;
+
+import java.util.Objects;
+
+public abstract class Parcel {
+    private final String description;
+    private final int weight;
+    private final String deliveryAddress;
+    private final int sendDay;
+
+    public Parcel (String description, int weight, String deliveryAddress, int sendDay) {
+        this.description = description;
+        this.weight = weight;
+        this.deliveryAddress = deliveryAddress;
+        this.sendDay = sendDay;
+    }
+
+
+
+    public void packageItem() {
+        System.out.println("Посылка, " + description + ", упакована.");
+    }
+
+    public void deliver() {
+        System.out.println("Посылка, " + description + ", доставлена по адресу: " + deliveryAddress + ".");
+    }
+
+    protected abstract int getDeliveryCostModifier();
+
+    public int calculateDeliveryCost() {
+        return weight * getDeliveryCostModifier();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getSendDay() {
+        return sendDay;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Parcel{"
+                + "description='" + description + "', "
+                + "weight=" + weight
+                + "deliveryAddress='" + deliveryAddress + "', "
+                + "sendDay=" + sendDay
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Parcel parcel = (Parcel) o;
+        return getWeight() == parcel.getWeight() &&
+                getSendDay() == parcel.getSendDay() &&
+                Objects.equals(getDescription(), parcel.getDescription()) &&
+                Objects.equals(deliveryAddress, parcel.deliveryAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDescription(), getWeight(), deliveryAddress, getSendDay());
+    }
+}
--- test/ru/yandex/practicum/DeliveryCostTest.java
+++ test/ru/yandex/practicum/DeliveryCostTest.java
@@ -1,4 +1,33 @@
 package ru.yandex.practicum;
 
+import ru.yandex.practicum.delivery.parcel.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class DeliveryCostTest {
+
+    @Test
+    public void shouldReturn20forStandardParcelOf10Kg() {
+        StandardParcel standardParcel = new StandardParcel("Кроссовки", 3, "Казань", 1);
+        int expectedResult = 20;
+        int actualResult = standardParcel.calculateDeliveryCost();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void shouldReturn30forPerishableParcelOf10Kg() {
+        PerishableParcel perishableParcel = new PerishableParcel("Молоко", 2, "Москва", 1, 5);
+        int expectedResult = 15;
+        int actualResult = perishableParcel.calculateDeliveryCost();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void shouldReturn40forFragileParcelOf10Kg() {
+        FragileParcel fragileParcel = new FragileParcel("Ваза", 3, "Тверь", 1);
+        int expectedResult = 8;
+        int actualResult = fragileParcel.calculateDeliveryCost();
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
 }
